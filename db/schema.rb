44ae612e4cc07d0593a6029f0b7b772b98de762d
Micha Bernhard <mib70@pitt.edu>
2021-12-15 15:01:44 -0700

    Feat: Add password digest column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_003427) do
+ActiveRecord::Schema.define(version: 2021_12_15_195836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_003427) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "parties", "users", column: "host_id"
