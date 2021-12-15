62e253cc3d6ff398c9a03876074638150de162b7
Micha Bernhard <mib70@pitt.edu>
2021-12-15 15:05:02 -0700

    Feat: Add validations and password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_many :user_parties
   has_many :parties, through: :user_parties
+
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_presence_of :password
+  validates_presence_of :password_confirmation
+  validates_presence_of :password_digest
+  validates_uniqueness_of :email
+
+  has_secure_password
 end
