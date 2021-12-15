5043e0f6847164f37c8617ceca18b0126a80020e
Micha Bernhard <mib70@pitt.edu>
2021-12-15 15:03:39 -0700

    Test: add password to user creation tests

--- spec/features/user/new_spec.rb
+++ spec/features/user/new_spec.rb
@@ -6,8 +6,48 @@ RSpec.describe 'new user register page' do
 
     fill_in :name, with: 'user'
     fill_in :email, with: 'email@email.com'
+    fill_in :password, with: 'password123'
+    fill_in :password_confirmation, with: 'password123'
     click_button 'Register'
 
-    expect(current_path).to eq(user_path(User.find_by email: 'email@email.com'))
+    user = User.find_by email: 'email@email.com'
+    
+    expect(current_path).to eq(user_path(user))
+  end
+
+  it 'redirects back to the registration page if passwords dont match' do
+    visit register_path
+
+    fill_in :name, with: 'user'
+    fill_in :email, with: 'email@email.com'
+    fill_in :password, with: 'password123'
+    fill_in :password_confirmation, with: 'p3'
+    click_button 'Register'
+    
+    expect(current_path).to eq(register_path)
+  end
+
+  it 'redirects back to the registration page if email is not unique' do
+    visit register_path
+
+    fill_in :name, with: 'user'
+    fill_in :email, with: 'email@email.com'
+    fill_in :password, with: 'password123'
+    fill_in :password_confirmation, with: 'password123'
+    click_button 'Register'
+    
+    user = User.find_by email: 'email@email.com'
+
+    expect(current_path).to eq(user_path(user))
+
+    visit register_path
+
+    fill_in :name, with: 'user'
+    fill_in :email, with: 'email@email.com'
+    fill_in :password, with: 'password123'
+    fill_in :password_confirmation, with: 'password123'
+    click_button 'Register'
+    
+    expect(current_path).to eq(register_path)
   end
 end
