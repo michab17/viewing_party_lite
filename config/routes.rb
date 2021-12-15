88c747d407383abd93bdcc5cac7d1058c70ca0ce
Micha Bernhard <mib70@pitt.edu>
2021-12-15 15:04:08 -0700

    Feat: Add login routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get '/register', to: 'users#new'
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login_user'
   resources :users, only: [:show, :create] do
     get '/discover', to: 'movies#index'
     get '/movies', to: 'movies#search'
