835fb3f993e92090b93f32d349a15d3030193948
Micha Bernhard <mib70@pitt.edu>
2021-12-15 15:05:20 -0700

    Feat: Add login to create, login methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,33 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(user_params)
-    redirect_to user_path(user)
+    if user.save
+      redirect_to user_path(user)
+    else
+      redirect_to register_path, notice: "Information you entered was invalid"
+    end
+  end
+
+  def login_form
+  end
+
+  def login_user
+    if User.find_by email: params[:email]
+      user = User.find_by email: params[:email]
+      if user.authenticate(params[:password])
+        redirect_to user_path(user)
+      else
+        redirect_to login_path, notice: "Email or password was incorrect"
+      end
+    else
+      redirect_to login_path, notice: "Email or password was incorrect"
+    end
   end
 
   private
 
   def user_params
-    params.permit(:name, :email)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
 
