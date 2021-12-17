c70123cc85b80f9c5d1f8a99069fd58b97ba06a8
Micha Bernhard <mib70@pitt.edu>
2021-12-16 18:18:09 -0700

    Change routes to handrolled and remove /user/id

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get '/register', to: 'users#new'
-  get '/login', to: 'users#login_form'
-  post '/login', to: 'users#login_user'
-  resources :users, only: [:show, :create] do
-    get '/discover', to: 'movies#index'
-    get '/movies', to: 'movies#search'
-    resources :movies, only: [:show] do
-      resources :parties, only: [:new, :create]
-    end
-  end
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/dashboard', to: 'users#show'
+  post '/users', to: 'users#create'
+
+  get '/discover', to: 'movies#index'
+  get '/movies', to: 'movies#search'
+  get '/movies/:id', to: 'movies#show'
+
+  post '/movies/:movie_id/parties', to: 'parties#create'
+  get '/movies/:movie_id/parties/new', to: 'parties#new'
+
+  # resources :movies do
+  #   resources :parties, only: [:create]
+  # end
+  resources :parties
 end
